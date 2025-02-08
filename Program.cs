3e9f5eff314fc21cdaac47f73d5bd92d35c2bb3e
EchoEclipseWolf <EchoEclipseWolf@gmail.com>
2025-02-07 19:05:44 -0600

    - Empty

--- Program.cs
+++ Program.cs
@@ -4,22 +4,9 @@ namespace YTMusicLocalSync
 {
     class Program
     {
-        public static Config config;
         static async Task Main(string[] args)
         {
-            config = new Config();
-            config.Load();
-            
-            API api = new API();
-            await api.Start(async () =>
-            {
-                List<string> playlists = api.GetPlaylists();
-                Console.WriteLine("User Playlists: ");
-                foreach(string item in playlists)
-                {
-                    Console.WriteLine(item);
-                }
-            });
+            Console.WriteLine("Empty");
         }
     }
 }
